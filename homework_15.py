06d4a2a20ed812864f3aa1b97acd41528bf3a4c5
FOVOWAGO <slanovmarat@mail.ru>
2024-12-16 02:09:40 +0300

    da
    da

--- homework_15.py
+++ homework_15.py
@@ -1,101 +1,100 @@
 import tkinter as tk
 
 
-# РЎРѕР·РґР°РЅРёРµ РіР»Р°РІРЅРѕРіРѕ РѕРєРЅР°
 def SOC(x, y, lmx, lmy, lpx, lpy, ex, ey, ox_color=None, oy_color=None):
     """
-        Р¤СѓРЅРєС†РёСЏ СЃРѕР·РґР°РЅРёСЏ Р”РџРЎРљ
-        РќР° РІС…РѕРґ РїСЂРёРЅРёРјР°РµС‚:
-        x, y - СЂР°Р·РјРµСЂС‹ С…РѕР»СЃС‚Р° (РІ РјРј)
-        lmx, lmy, lpx, lpy - РґР»РёРЅС‹ РѕСЃРµР№ Р°Р±СЃС†РёСЃСЃС‹ Рё РѕСЂРґРёРЅР°С‚С‹ РІ СЃРѕРѕС‚РІРµС‚СЃС‚РІСѓС‰РёС… РЅР°РїСЂР°РІР»РµРЅРёСЏС… (РІ СЃРј)
-        ex, ey - РµРґРёРЅРёС‡РЅС‹Рµ РѕС‚СЂРµР·РєРё РЅР° РѕСЃСЏС… X Рё Y СЃРѕРѕС‚РІРµС‚СЃС‚РІРµРЅРЅРѕ
-        ox_color, oy_color (РЅРµРѕР±СЏР·Р°С‚РµР»СЊРЅС‹Рµ Р°СЂРіСѓРјРµРЅС‚С‹) - С†РІРµС‚Р° РѕСЃРµР№ X Рё Y СЃРѕРѕС‚РІРµС‚СЃС‚РІРµРЅРЅРѕ
+        Функция создания ДПСК
+        На вход принимает:
+        x, y - размеры холста (в мм)
+        lmx, lmy, lpx, lpy - длины осей абсциссы и ординаты в соответствущих направлениях (в см)
+        ex, ey - единичные отрезки на осях X и Y соответственно
+        ox_color, oy_color (необязательные аргументы) - цвета осей X и Y соответственно
     """
-    # РќР°С‡Р°Р»Рѕ РєРѕРѕСЂРґРёРЅР°С‚
+    # Начало координат
     xc = x / 2
     yc = y / 2
 
-    # РјР°СЃС€С‚Р°Р±РёСЂСѓСЋС‰РёР№ РјРЅРѕР¶РёС‚РµР»СЊ
+    # масштабирующий множитель
     nm = 10
 
-    # С†РµРЅР° РґРµР»РµРЅРёСЏ РІ РїРёРєСЃРµР»СЏС…/РјРј
+    # цена деления в пикселях/мм
     xnm = ex * nm
     ynm = ey * nm
 
-    # РќР°СЃС‚СЂРѕР№РєРё Рё СЃРѕР·РґР°РЅРёРµ С…РѕР»СЃС‚Р°
+    # Настройки и создание холста
     root = tk.Tk()
     root.title("Coordinate system")
     root.geometry(f"{x}x{y}")
     canvas = tk.Canvas(bg='white', width=x, height=y)
     canvas.pack()
 
-    # РЎРѕР·РґР°РЅРёРµ Р»РёРЅРёР№ РѕСЃРµР№
-    # x РІ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹С… Р·РЅР°С‡РµРЅРёСЏС…
+    # Создание линий осей
+    # x в отрицательных значениях
     canvas.create_line(xc - lmx - 5, yc, xc, yc, fill=ox_color)
-    # y РІ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅС‹С… Р·РЅР°С‡РµРЅРёСЏС…
+    # y в отрицательных значениях
     canvas.create_line(xc, yc + lmy + 5, xc, yc, fill=oy_color)
-    # x РІ РїРѕР»РѕР¶РёС‚РµР»СЊРЅС‹С… Р·РЅР°С‡РµРЅРёСЏС…
+    # x в положительных значениях
     canvas.create_line(xc, yc, xc + lpx, yc, fill=ox_color)
-    # y РІ РїРѕР»РѕР¶РёС‚РµР»СЊРЅС‹С… Р·РЅР°С‡РµРЅРёСЏС…
+    # y в положительных значениях
     canvas.create_line(xc, yc, xc, yc - lpy, fill=oy_color)
 
-    # РЎРѕР·РґР°РЅРёРµ СЃС‚СЂРµР»РѕРє
+    # Создание стрелок
     canvas.create_line(xc + lpx - 10, yc - 7, xc + lpx, yc, fill=ox_color)
     canvas.create_line(xc + lpx - 10, yc + 7, xc + lpx, yc, fill=ox_color)
     canvas.create_line(xc, yc - lpy, xc - 7, yc - lpy + 10, fill=oy_color)
     canvas.create_line(xc, yc - lpy, xc + 7, yc - lpy + 10, fill=oy_color)
 
-    # РЎРѕР·РґР°РЅРёРµ РґРµР»РµРЅРёР№
-    # РґРµР»РµРЅРёСЏ РЅР° РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРј РЅР°РїСЂР°РІР»РµРЅРёРё РѕСЃРё x
+    # Создание делений
+    # деления на отрицательном направлении оси x
     xnowm = xc + nm
     for i in range(lmx // xnm):
         canvas.create_line(xnowm - xnm, yc - 4, xnowm - xnm, yc + 5, fill=ox_color)
         xnowm -= xnm
-    # РґРµР»РµРЅРёСЏ РЅР° РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРј РЅР°РїСЂР°РІР»РµРЅРёРё РѕСЃРё y
+    # деления на отрицательном направлении оси y
     ynowm = yc
     for i in range(lmy // ynm):
         canvas.create_line(xc - 4, ynowm + ynm, xc + 5, ynowm + ynm, fill=oy_color)
         ynowm += ynm
-    # РґРµР»РµРЅРёСЏ РЅР° РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРј РЅР°РїСЂР°РІР»РµРЅРёРё РѕСЃРё x
+    # деления на положительном направлении оси x
     xnowp = xc - nm
     for i in range((lpx // xnm) - 1):
         canvas.create_line(xnowp + xnm, yc - 4, xnowp + xnm, yc + 5, fill=ox_color)
         xnowp += xnm
-    # РґРµР»РµРЅРёСЏ РЅР° РїРѕР»РѕР¶РёС‚РµР»СЊРЅРѕРј РЅР°РїСЂР°РІР»РµРЅРёРё РѕСЃРё y
+    # деления на положительном направлении оси y
     ynowp = yc
     for i in range((lpy // ynm) - 1):
         canvas.create_line(xc - 4, ynowp - ynm, xc + 5, ynowp - ynm, fill=oy_color)
         ynowp -= ynm
 
-    # РќР°С‡Р°Р»Рѕ РєРѕРѕСЂРґРёРЅР°С‚
+    # Начало координат
     canvas.create_text(xc - nm, yc + nm, text='0')
-    # РЎРёРјРІРѕР»С‹ X Рё Y
+    # Символы X и Y
     canvas.create_text(xc + lpx, yc + nm, text='X')
     canvas.create_text(xc + nm, yc - lpy, text='Y')
 
-    # С†РµРЅР° РґРµР»РµРЅРёСЏ
-    # РЅР° +x
+    # цена деления
+    # на +x
     xnowp = xc - nm
     enx = ex
     for i in range((lpx // xnm) - 1):
         canvas.create_text(xnowp + xnm, yc + 10, text=f'{enx}')
         xnowp += xnm
         enx += ex
-    # РЅР° +y
+    # на +y
     ynowp = yc
     eny = ey
     for i in range((lpy // ynm) - 1):
         canvas.create_text(xc + 15, ynowp - ynm, text=f'{eny}')
         ynowp -= ynm
         eny += ey
-    # РЅР° -x
+    # на -x
     xnowm = xc + nm
     enx = -1 * ex
     for i in range(lmx // xnm):
         canvas.create_text(xnowm - xnm, yc + 10, text=f'{enx}')
         xnowm -= xnm
         enx -= ex
-    # РЅР° -y
+    # на -y
     ynowm = yc
     enx = -1 * ey
     for i in range(lmy // ynm):
